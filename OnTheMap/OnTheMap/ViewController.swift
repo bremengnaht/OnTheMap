06772e94d56f32cb12602cf85c629ab739166be5
Thang Nguyen <bremengnaht@ThangNQ16.local>
2024-07-13 11:10:31 +0700

    Init

---
+++ OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Nguyen Quyet Thang on 13/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
