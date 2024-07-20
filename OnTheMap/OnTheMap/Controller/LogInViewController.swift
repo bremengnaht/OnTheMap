cbf64350c1149b2b257a99bd3875097f34a25665
Thang Nguyen <bremengnaht@ThangNQ16.local>
2024-07-20 11:13:37 +0700

    Update Login Screen

--- OnTheMap/OnTheMap/Controller/LogInViewController.swift
+++ OnTheMap/OnTheMap/Controller/LogInViewController.swift
@@ -1,19 +1,17 @@
 //
-//  ViewController.swift
+//  LogInViewController.swift
 //  OnTheMap
 //
-//  Created by Nguyen Quyet Thang on 13/7/24.
+//  Created by Nguyen Quyet Thang on 20/7/24.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class LogInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
