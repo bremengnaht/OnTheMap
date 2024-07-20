6d2c52744399865dc085fcda0c16937acd01c80c
Thang Nguyen <bremengnaht@ThangNQ16.local>
2024-07-20 09:59:12 +0700

    Add asset

---
+++ OnTheMap/OnTheMap/Controller/StudentLocations.swift
@@ -0,0 +1,12 @@
+//
+//  StudentLocations.swift
+//  OnTheMap
+//
+//  Created by Nguyen Quyet Thang on 20/7/24.
+//
+
+import Foundation
+
+struct StudentLocations: Codable {
+    let results: [StudentInfo]
+}
