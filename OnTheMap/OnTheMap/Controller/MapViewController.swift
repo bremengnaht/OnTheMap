638dccaf88182e2c46f9b6e0dd494274376d8391
Thang Nguyen <bremengnaht@ThangNQ16.local>
2024-07-20 15:10:12 +0700

    Update layout

---
+++ OnTheMap/OnTheMap/Controller/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Nguyen Quyet Thang on 20/7/24.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+}
