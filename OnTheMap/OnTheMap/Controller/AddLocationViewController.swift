47e7b9db3919fffab1af8486d576651a12815399
Thang Nguyen <bremengnaht@ThangNQ16.local>
2024-07-22 00:05:37 +0700

    Comment 6

--- OnTheMap/OnTheMap/Controller/AddLocationViewController.swift
+++ OnTheMap/OnTheMap/Controller/AddLocationViewController.swift
@@ -11,11 +11,13 @@ import MapKit
 class AddLocationViewController: UIViewController {
     @IBOutlet weak var txtLocation: UITextField!
     @IBOutlet weak var txtLink: UITextField!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
     
     var request: CreateStudentLocationRequest?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.stopAnimating()
     }
     
     @IBAction func cancel(_ sender: Any) {
@@ -23,19 +25,29 @@ class AddLocationViewController: UIViewController {
     }
     
     @IBAction func findLocation(_ sender: Any) {
-        guard let locationText = txtLocation.text else { return }
-        guard let linkText = txtLink.text else { return }
+        activityIndicator.startAnimating()
+        guard let locationText = txtLocation.text else {
+            self.activityIndicator.stopAnimating()
+            return
+        }
+        guard let linkText = txtLink.text else {
+            self.activityIndicator.stopAnimating()
+            return
+        }
         guard locationText != "" && linkText != "" else {
+            self.activityIndicator.stopAnimating()
             showAlert(title: "Alert !", message: "Can't leave empty")
             return
         }
         guard isValidURL(linkText) else {
+            self.activityIndicator.stopAnimating()
             showAlert(title: "Alert !", message: "Link is not valid")
             return
         }
         
         CLGeocoder().geocodeAddressString(locationText) { (newMarker, error) in
             if error != nil {
+                self.activityIndicator.stopAnimating()
                 self.showAlert(title: "Error", message: "Location Not Found")
             } else {
                 var location: CLLocation?
@@ -44,8 +56,10 @@ class AddLocationViewController: UIViewController {
                 }
                 if let location = location {
                     self.request = CreateStudentLocationRequest(firstName: "Test", lastName: "User", latitude: location.coordinate.latitude, longitude: location.coordinate.longitude, mapString: locationText, mediaURL: linkText, uniqueKey: UUID().uuidString)
+                    self.activityIndicator.stopAnimating()
                     self.performSegue(withIdentifier: "addLocationFinalStep", sender: nil)
                 } else {
+                    self.activityIndicator.stopAnimating()
                     self.showAlert(title: "Alert !", message: "Location do not exists")
                 }
             }
