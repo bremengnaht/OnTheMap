c2b3f94b89e375c831c76d35fb11e4b06fcea61e
Thang Nguyen <bremengnaht@ThangNQ16.local>
2024-07-21 15:38:04 +0700

    Update

--- OnTheMap/OnTheMap/Controller/ListViewController.swift
+++ OnTheMap/OnTheMap/Controller/ListViewController.swift
@@ -7,23 +7,23 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return getSharedDatasource().count
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeCellTableIndentifier", for: indexPath) as! CustomTableViewCell
+//        
+//        let data = getSharedDatasource()[indexPath.row]
+//        cell.customImageView.image = data.memedImage
+//        cell.customDisplayName.text = "\(data.topText ?? "") \(data.bottomText ?? "")"
+//        
+//        return cell
+//    }
 }
---
+++ OnTheMap/OnTheMap/Model/CustomUITableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomUITableViewCell.swift
+//  OnTheMap
+//
+//  Created by Nguyen Quyet Thang on 21/7/24.
+//
+
+import UIKit
+
+class CustomUITableViewCell: UITableViewCell {
+    @IBOutlet weak var txtTitle: UILabel!
+    @IBOutlet weak var txtSubtitle: UILabel!
+}
